f66c71cd834e16d5f1cb1760be6a810e8db43c50
Villarruel Thiago Bautista <thiagobautistavillarruel@hotmail.com>
2023-08-31 08:12:49 -0300

    Se añadieron los controladores de las solicitudes HTTP y se crearon los endpoints

--- src/main/java/com/exampleproject/employeecrud/Controller/EmployeeController.java
+++ src/main/java/com/exampleproject/employeecrud/Controller/EmployeeController.java
@@ -1,12 +1,56 @@
 package com.exampleproject.employeecrud.Controller;
 
+import com.exampleproject.employeecrud.Dto.EmployeeDTO;
+import com.exampleproject.employeecrud.Models.Entities.Employee;
+import com.exampleproject.employeecrud.Models.Gender;
+import com.exampleproject.employeecrud.Services.Implementation.EmployeeServiceImpl;
+
 import org.springframework.web.bind.annotation.*;
 
-
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/employees")
 public class EmployeeController{
 
+    private final EmployeeServiceImpl employeeServiceImpl;
+
+    public EmployeeController(EmployeeServiceImpl employeeServiceImpl) {
+        this. employeeServiceImpl = employeeServiceImpl;
+    }
+
+    @PostMapping("/")
+    public EmployeeDTO saveEmployee(@RequestBody Employee employee){
+        return employeeServiceImpl.saveEmployee(employee);
+    }
+
+    @GetMapping("/{id}")
+    public EmployeeDTO getEmployeeById(@PathVariable Long id) {
+        return employeeServiceImpl.getEmployeeById(id);
+    }
+
+    @GetMapping("/")
+    public List<EmployeeDTO> getAllEmployees(){
+        return employeeServiceImpl.getAllEmployees();
+    }
+
+    @GetMapping("/gender/{gender}")
+    public List<EmployeeDTO> getEmployeesByGender(@PathVariable Gender gender) {
+        return employeeServiceImpl.getEmployeesByGender(gender);
+    }
+
+    @PutMapping("/{id}")
+    public EmployeeDTO updateEmployeeById(@PathVariable Long id, @RequestBody Employee employee){
+        return employeeServiceImpl.updateEmployeeById(id, employee);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEmployeeById(@PathVariable Long id){
+        employeeServiceImpl.deleteEmployeeById(id);
+    }
+    @DeleteMapping("/")
+    public void deleteEmployeeById(){
+        employeeServiceImpl.deleteAllEmployees();
+    }
 
 }
