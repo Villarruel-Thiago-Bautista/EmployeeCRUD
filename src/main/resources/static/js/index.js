e5394f7857e0a4f04a1ab2c736f2be01aeb9ed69
Villarruel Thiago Bautista <thiagobautistavillarruel@hotmail.com>
2023-08-31 18:26:29 -0300

    Se añadio la funcionalidad del metodo PUT y de los metodos DELETE

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,10 +1,10 @@
 /* Constantes de los botones */
 
 const postEmployeeButton = document.getElementById("postEmployeeButton");
-const putEmployeeButton = document.getElementById("putEmployeeButtonButton");
-const getEmployeeByIdButton = document.getElementById("getEmployeeByIdButtonByIdButton");
-const getEmployeeByGenderButton = document.getElementById("getEmployeeByGenderButtonByGenderButton");
-const getAllEmployeesButton = document.getElementById("getEmployeeByIdButtonAllButton");
+const putEmployeeButton = document.getElementById("putEmployeeButton");
+const getEmployeeByIdButton = document.getElementById("getEmployeeByIdButton");
+const getEmployeeByGenderButton = document.getElementById("getEmployeeByGenderButton");
+const getAllEmployeesButton = document.getElementById("getEmployeeByIdButton");
 const deleteEmployeByIdButton = document.getElementById("deleteEmployeeByIdButton");
 const deleteAllEmployeesButton = document.getElementById("deleteAllEmployeesButton");
 
@@ -44,7 +44,7 @@ function saveEmployee() {
         body: JSON.stringify(postRequest)
     };
 
-    /*Funcion fetch*/
+    /* Funcion fetch */
 
     fetch(generalEmployeeEndpoint, postOptions)
         .then(response => response.json())
@@ -56,13 +56,173 @@ function saveEmployee() {
             const genderData = data.gender;
             const dniData = data.dni;
             printOnTheScreen(idData, nameData, lastNameData, emailData, genderData, dniData);
+        })
+        .catch(error => {
+            console.error('Error:', error);
         });
 }
-/* Añado al botón un evento click y mediante una funcion llama a la funcion saveEmployee */
+
+/* Metodo put */
+
+function updateEmployeeById(){
+
+    /* Constantes de los valores de los inputs para modificar */
+
+    const employeeIdUpdated = document.getElementById("put-id").value;
+    const employeeNameUpdated = document.getElementById("put-name").value;
+    const employeeLastNameUpdated = document.getElementById("put-lastName").value;
+    const employeeEmailUpdated = document.getElementById("put-email").value;
+    const employeeGenderUpdated = document.getElementById("put-gender").value;
+    const employeeDniUpdated = document.getElementById("put-dni").value;
+
+    /* Endpoint */
+
+    const employeeByIdEndpoint = `http://localhost:8080/api/v1/employees/${employeeIdUpdated}`;
+
+    /* Json donde voy a guardar los datos modificados */
+
+    const putRequest = {
+        name: employeeNameUpdated,
+        lastName: employeeLastNameUpdated,
+        email: employeeEmailUpdated,
+        gender: employeeGenderUpdated,
+        dni: employeeDniUpdated
+    };
+
+    /* Opciones del metodo Put */
+
+    const putOptions = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(putRequest)
+    };
+
+    /* Funcion fetch */
+
+    fetch(employeeByIdEndpoint, putOptions)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            updateOnTheScreen(employeeIdUpdated, employeeNameUpdated, employeeLastNameUpdated, employeeEmailUpdated, employeeGenderUpdated, employeeDniUpdated);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+/* Metodos DELETE */
+
+function deleteEmployeById() {
+
+    /* Constante del valor del id ingresado */
+
+    const deleteId = document.getElementById("delete-id").value;
+
+    /* Endpoint */
+
+    const employeeByIdEndpoint = `http://localhost:8080/api/v1/employees/${deleteId}`;
+
+    /* Opciones del metodo DELETE */
+
+    const deleteOptions = {
+        method: "DELETE"
+    };
+
+    /* Funcion fetch */
+    fetch(employeeByIdEndpoint, deleteOptions)
+        .then(response => {
+            if (response.ok) {
+                const table = document.getElementById("table-employees").getElementsByTagName("tbody")[0];
+                const rows = table.getElementsByTagName("tr");
+
+                for (let i = 0; i < rows.length; i++) {
+                    const trId = rows[i].getElementsByTagName("td")[0];
+                    if (trId.textContent === deleteId) {
+                        table.removeChild(rows[i]); // Elimina la fila correspondiente
+                        break;
+                    }
+                }
+            } else {
+                console.error('Error: no existe un empleado con el id especificado', response.statusText);
+            }
+        })
+        .catch(error => {
+            console.error('Error: no existe un empleado con el id especificado', error);
+        });
+}
+
+function deleteAllEmployees() {
+
+    /* Endpoint */
+
+    const generalEmployeeEndpoint = "http://localhost:8080/api/v1/employees/";
+
+    /* Opciones del metodo DELETE */
+
+    const deleteOptions = {
+        method: "DELETE"
+    };
+
+    /* Funcion fetch */
+
+    fetch(generalEmployeeEndpoint, deleteOptions)
+        .then(response => {
+            if (response.ok) {
+                const table = document.getElementById("table-employees").getElementsByTagName("tbody")[0];
+                table.innerHTML = "";
+
+            } else {
+                console.error('Error: no existe un empleado con el id especificado', response.statusText);
+            }
+        })
+        .catch(error => {
+            console.error('Error: no existe un empleado con el id especificado', error);
+        });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Botones que llaman a los metodos */
 
 postEmployeeButton.addEventListener("click", function (){
     saveEmployee();
-})
+});
+
+putEmployeeButton.addEventListener("click", function () {
+    updateEmployeeById();
+});
+
+deleteEmployeByIdButton.addEventListener("click", function () {
+    deleteEmployeById();
+});
+
+deleteAllEmployeesButton.addEventListener("click", function () {
+    deleteAllEmployees();
+});
+
+/* Funciones que trabajan los datos mostrados en pantalla */
+
+/* Funcion que añade a la tabla los datos de los empleados cargados en la DB */
 
 function printOnTheScreen(idData, nameData, lastNameData, emailData, genderData, dniData) {
 
@@ -92,10 +252,26 @@ function printOnTheScreen(idData, nameData, lastNameData, emailData, genderData,
     table.appendChild(newRow);
 }
 
+/* Funcion que actualiza la informacion en pantalla de la tabla con los datos modificados */
 
+function updateOnTheScreen(id, name, lastName, email, gender, dni){
 
+    const table = document.getElementById("table-employees").getElementsByTagName("tbody")[0];
+    const rows = table.getElementsByTagName("tr");
 
-
+    for (let i = 0; i< rows.length; i++) {
+        const trId = rows[i].getElementsByTagName("td")[0];
+        if(trId.textContent === id) {
+            const cells = rows[i].getElementsByTagName("td");
+            cells[1].textContent = name;
+            cells[2].textContent = lastName;
+            cells[3].textContent = email;
+            cells[4].textContent = gender;
+            cells[5].textContent = dni;
+            break;
+        }
+    }
+}
 
 
 
@@ -105,20 +281,8 @@ function printOnTheScreen(idData, nameData, lastNameData, emailData, genderData,
 
 Endpoints
 
+const generalEmployeeEndpoint = "http://localhost:8080/api/v1/employees/";
 const employeeByIdEndpoint = `http://localhost:8080/api/v1/employees/${id}`;
 const getEmployeeByGenderEndpoint =  `http://localhost:8080/api/v1/employees/gender/${gender}`;
 */
 
-
-/*
-
-Constantes de los valores de los inputs para modificar
-
-const employeeIdUpdated = document.getElementById("put-id").value;
-const employeeNameUpdated = document.getElementById("put-name").value;
-const employeeLastNameUpdated = document.getElementById("put-lastName").value;
-const employeeEmailUpdated = document.getElementById("put-email").value;
-const employeeGenderUpdated = document.getElementById("put-gender").value;
-const employeeDniUpdated =document.getElementById("put-dni").value;
-
-*/
