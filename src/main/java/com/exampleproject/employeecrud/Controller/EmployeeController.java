0525269b15a9063bbb756dc28df5d5607b7a16f8
Villarruel Thiago Bautista <thiagobautistavillarruel@hotmail.com>
2023-09-06 19:24:30 -0300

    Se agregó documentación por Swagger y la dependencia correspondiente

--- src/main/java/com/exampleproject/employeecrud/Controller/EmployeeController.java
+++ src/main/java/com/exampleproject/employeecrud/Controller/EmployeeController.java
@@ -5,6 +5,11 @@ import com.exampleproject.employeecrud.Models.Entities.Employee;
 import com.exampleproject.employeecrud.Models.Gender;
 import com.exampleproject.employeecrud.Services.Implementation.EmployeeServiceImpl;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,36 +26,69 @@ public class EmployeeController{
     }
 
     @PostMapping("/")
+    @Operation(summary = "Insertar Persona", description = "Inserta una nueva persona.")
+    @ApiResponse(responseCode = "200", description = "Persona insertada exitosamente", content = @Content(mediaType = "application/json", schema = @Schema(implementation = EmployeeDTO.class)))
+    @ApiResponse(responseCode = "400", description = "Error en la solicitud")
+    @ApiResponse(responseCode = "500", description = "Error interno del servidor")
     public EmployeeDTO saveEmployee(@Valid @RequestBody Employee employee){
         return employeeServiceImpl.saveEmployee(employee);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Obtener Persona por ID", description = "Obtiene una persona por su ID.")
+    @ApiResponse(responseCode = "200", description = "Persona encontrada", content = @Content(mediaType = "application/json", schema = @Schema(implementation = EmployeeDTO.class)))
+    @ApiResponse(responseCode = "400", description = "Error en la solicitud")
+    @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+    @ApiResponse(responseCode = "404", description = "Persona no encontrada")
     public EmployeeDTO getEmployeeById(@PathVariable Long id) {
         return employeeServiceImpl.getEmployeeById(id);
     }
 
     @GetMapping("/")
+    @Operation(summary = "Obtener Todas las Personas", description = "Obtiene todas las personas.")
+    @ApiResponse(responseCode = "200", description = "Lista de todas las personas", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = EmployeeDTO.class))))
+    @ApiResponse(responseCode = "400", description = "Error en la solicitud")
+    @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+    @ApiResponse(responseCode = "404", description = "Lista de personas vacía")
     public List<EmployeeDTO> getAllEmployees(){
         return employeeServiceImpl.getAllEmployees();
     }
 
     @GetMapping("/gender/{gender}")
+    @Operation(summary = "Obtener Lista de Personas por Género", description = "Obtiene una lista de personas por su género.")
+    @ApiResponse(responseCode = "200", description = "Lista de personas por género", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = EmployeeDTO.class))))
+    @ApiResponse(responseCode = "400", description = "Error en la solicitud")
+    @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+    @ApiResponse(responseCode = "404", description = "Persona no encontrada por género")
     public List<EmployeeDTO> getEmployeesByGender(@PathVariable Gender gender) {
         return employeeServiceImpl.getEmployeesByGender(gender);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Actualizar Persona", description = "Actualiza una persona existente por su ID.")
+    @ApiResponse(responseCode = "200", description = "Persona actualizada exitosamente", content = @Content(mediaType = "application/json", schema = @Schema(implementation = EmployeeDTO.class)))
+    @ApiResponse(responseCode = "400", description = "Error en la solicitud")
+    @ApiResponse(responseCode = "500", description = "Error interno del servidor")
     public EmployeeDTO updateEmployeeById(@Valid @PathVariable Long id, @RequestBody Employee employee){
         return employeeServiceImpl.updateEmployeeById(id, employee);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Eliminar Persona por ID", description = "Elimina una persona por su ID.")
+    @ApiResponse(responseCode = "200", description = "Persona eliminada exitosamente")
+    @ApiResponse(responseCode = "400", description = "Error en la solicitud")
+    @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+    @ApiResponse(responseCode = "404", description = "Persona no encontrada")
     public void deleteEmployeeById(@PathVariable Long id){
         employeeServiceImpl.deleteEmployeeById(id);
     }
 
     @DeleteMapping("/")
+    @Operation(summary = "Eliminar Todas las Personas", description = "Elimina todas las personas.")
+    @ApiResponse(responseCode = "200", description = "Todas las personas eliminadas exitosamente")
+    @ApiResponse(responseCode = "400", description = "Error en la solicitud")
+    @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+    @ApiResponse(responseCode = "404", description = "Lista de personas vacía")
     public void deleteEmployeeById(){
         employeeServiceImpl.deleteAllEmployees();
     }
--- src/main/java/com/exampleproject/employeecrud/Models/Entities/Employee.java
+++ src/main/java/com/exampleproject/employeecrud/Models/Entities/Employee.java
@@ -1,6 +1,7 @@
 package com.exampleproject.employeecrud.Models.Entities;
 
 import com.exampleproject.employeecrud.Models.Gender;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
@@ -26,39 +27,46 @@ public class Employee {
     @NotBlank(message = "El campo no puede estar vacio")
     @NotNull(message = "El campo no puede ser nulo")
     @Pattern(regexp = "^[a-zA-Z]+$", message = "El campo nombre no puede contener números ni caracteres especiales")
+    @Schema(description = "Nombre del empleado", example = "John")
     private String name;
 
     @Column(name = "lastName", nullable = false)
     @NotBlank(message = "El campo no puede estar vacio")
     @NotNull(message = "El campo no puede ser nulo")
     @Pattern(regexp = "^[a-zA-Z]+$", message = "El campo nombre no puede contener números ni caracteres especiales")
+    @Schema(description = "Apellido del empleado", example = "Doe")
     private String lastName;
 
     @Column(name = "age", nullable = false)
     @Positive(message = "Debe ser positivo y no nulo")
     @NotNull(message = "El campo no puede ser nulo")
+    @Schema(description = "Edad del empleado", example = "30")
     private Integer age;
 
     @Column(name = "email", nullable = false)
     @NotBlank(message = "El campo no puede estar vacio")
     @NotNull(message = "El campo no puede ser nulo")
     @Email(message = "El campo debe contener un formato de direccion de correo electronico valido")
+    @Schema(description = "Correo electrónico del empleado", example = "example@example.com")
     private String email;
 
     @Column(name = "cellphone", nullable = false)
     @Positive(message = "Debe ser positivo y no nulo")
     @NotNull(message = "El campo no puede ser nulo")
+    @Schema(description = "Número de teléfono del empleado", example = "123456789")
     private Long cellphone;
 
     @Column(name = "gender", nullable = false)
     @NotNull(message = "El campo no puede ser nulo")
     @Enumerated(value = EnumType.STRING)
+    @Schema(description = "Género del empleado", example = "MASCULINO")
     private Gender gender;
 
     @Column(name = "dni", nullable = false)
     @NotBlank(message = "El campo no puede estar vacio")
     @NotNull(message = "El campo no puede ser nulo")
     @Pattern(regexp = "^[0-9]{8}$", message = "El campo DNI debe contener 8 dígitos numéricos")
+    @Schema(description = "DNI del empleado", example = "12345678")
     private String dni;
 
 
